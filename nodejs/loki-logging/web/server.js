b880af2286bf0a52c4bfdbb29682e7dbc3f7d9d9
up1 <somkiat.p@gmail.com>
2023-02-02 01:01:54 +0700

    demo logging with loki

---
+++ nodejs/loki-logging/web/server.js
@@ -0,0 +1,42 @@
+const { createLogger, transports } = require("winston");
+const LokiTransport = require("winston-loki");
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+// Home page
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname, 'index.html')));
+});
+
+// Logger
+const logger = createLogger({
+  transports: [
+    new LokiTransport({
+      host: "http://loki:3100",
+      // Only for development
+      interval: 5,
+      labels: {
+        job: 'nodejs'
+      }
+    })
+  ]
+})
+
+// Send random logs repeatedly
+setInterval(() => {
+  const level = getRandomArrayElement(['debug', 'info', 'warn', 'error']);
+  const labels = getRandomArrayElement([{ env: 'dev' }, { env: 'prod' }]);
+  const message = getRandomArrayElement(['This is just some log message...', 'Oh snap! Something went wrong.']);
+  logger[level]({ message, labels })
+}, 2000);
+
+// Start the webserver
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+function getRandomArrayElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
